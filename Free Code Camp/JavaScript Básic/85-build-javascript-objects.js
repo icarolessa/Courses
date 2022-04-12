89d4cdf0ca4a65ffcff1fce3a6824d58ce58dd51
icarolessa <icaro.ld92@gmail.com>
2022-04-12 18:29:34 -0300

    Free Code Camp

---
+++ Free Code Camp/JavaScript Básic/85-build-javascript-objects.js
@@ -0,0 +1,8 @@
+const myDog = {
+  // Only change code below this line
+  name: 'Matilda',
+  legs: 4,
+  tails: 1,
+  friends: ['Cerafina', 'Mafalda', 'Olivia']
+  // Only change code above this line
+};
