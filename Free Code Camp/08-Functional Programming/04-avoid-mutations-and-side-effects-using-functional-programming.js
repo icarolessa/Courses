5610fc012d94a71a037e148c81d94049b60d3093
icarolessa <icaro.ld92@gmail.com>
2022-04-20 19:32:42 -0300

    Functional Programming

---
+++ Free Code Camp/08-Functional Programming/04-avoid-mutations-and-side-effects-using-functional-programming.js
@@ -0,0 +1,9 @@
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  let basedValue = fixedValue
+  // Only change code below this line
+  return basedValue += 1
+  // Only change code above this line
+}
